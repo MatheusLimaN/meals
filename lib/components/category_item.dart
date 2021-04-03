c3812303017c897f1a694e43c2b0a6347cdf4ce3
Matheus Lima do Nascimento <matheuslima.n@hotmail.com>
2021-04-03 16:19:49 -0300

    initial commit

---
+++ lib/components/category_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/utils/app_routes.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem({Key key, this.category}) : super(key: key);
+
+  void _selectCategory(BuildContext context) {
+    // Navigator.of(context).push(
+    //   MaterialPageRoute(builder: (_) {
+    //     return CategoriesMealsScreen(
+    //       category: category,
+    //     );
+    //   }),
+    // );
+    Navigator.of(context)
+        .pushNamed(AppRoutes.CATEGORES_MEALS, arguments: category);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => _selectCategory(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+              colors: [category.color.withOpacity(0.5), category.color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+      ),
+    );
+  }
+}
